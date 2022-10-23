1ef240db9261a74f06a2910a0228012b46784898
Gregory Petrosyan <pgregory@pgregory.net>
2022-10-23 22:23:53 +0300

    Add lalrpop

---
+++ crates/fennec-parser/build.rs
@@ -0,0 +1,11 @@
+// Copyright 2022 Gregory Petrosyan <pgregory@pgregory.net>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+extern crate lalrpop;
+
+fn main() {
+    lalrpop::process_root().unwrap();
+}
--- crates/fennec-parser/src/lib.rs
+++ crates/fennec-parser/src/lib.rs
@@ -1,14 +1,18 @@
-pub fn parse(_code: &str) -> i64 {
-    42
-}
+// Copyright 2022 Gregory Petrosyan <pgregory@pgregory.net>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[macro_use]
+extern crate lalrpop_util;
 
-    #[test]
-    fn parse_42() {
-        let result = parse("");
-        assert_eq!(result, 42);
-    }
+lalrpop_mod!(pub example);
+
+#[test]
+fn lalrpop_example() {
+    assert!(example::TermParser::new().parse("22").is_ok());
+    assert!(example::TermParser::new().parse("(22)").is_ok());
+    assert!(example::TermParser::new().parse("((((22))))").is_ok());
+    assert!(example::TermParser::new().parse("((22)").is_err());
 }
