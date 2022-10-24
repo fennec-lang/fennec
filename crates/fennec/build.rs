e44c68bf1afc9c48ca35da380516bf0c74e9bcc1
Gregory Petrosyan <pgregory@pgregory.net>
2022-10-25 00:28:01 +0300

    Better build script

--- crates/fennec/build.rs
+++ crates/fennec/build.rs
@@ -7,20 +7,31 @@
 use std::process::Command;
 
 fn main() -> anyhow::Result<()> {
-    let date_out = Command::new("date").args(&["--rfc-email"]).output()?;
-    let date = String::from_utf8(date_out.stdout)?;
+    let date = Command::new("date").args(&["--rfc-email"]).output()?;
+    let date = String::from_utf8(date.stdout)?;
 
-    let git_describe_out = Command::new("git")
+    let git_describe = Command::new("git")
         .args(&["describe", "--tags", "--dirty"])
         .output()?;
-    let git_describe = String::from_utf8(git_describe_out.stdout)?;
+    let git_describe = String::from_utf8(git_describe.stdout)?;
 
-    let rustc_version_out = Command::new("rustc").args(&["--version"]).output()?;
-    let rustc_version = String::from_utf8(rustc_version_out.stdout)?;
+    let git_branch = Command::new("git")
+        .args(&["branch", "--show-current"])
+        .output()?;
+    let git_branch = String::from_utf8(git_branch.stdout)?;
+
+    let rustc_version = Command::new("rustc").args(&["--version"]).output()?;
+    let rustc_version = String::from_utf8(rustc_version.stdout)?;
 
     println!("cargo:rustc-env=BUILD_DATE={}", date);
     println!("cargo:rustc-env=BUILD_GIT_DESCRIBE={}", git_describe);
     println!("cargo:rustc-env=BUILD_RUSTC_VERSION={}", rustc_version);
+    println!("cargo:rerun-if-changed=../../crates/");
+    println!("cargo:rerun-if-changed=../../.git/HEAD");
+    println!(
+        "cargo:rerun-if-changed=../../.git/refs/heads/{}",
+        git_branch
+    );
 
     Ok(())
 }
