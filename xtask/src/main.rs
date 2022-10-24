081b61794f7ac0c1820348d7023c59d3602b427a
Gregory Petrosyan <pgregory@pgregory.net>
2022-10-24 23:22:29 +0300

    Set up cargo xtask

---
+++ xtask/src/main.rs
@@ -0,0 +1,19 @@
+// Copyright 2022 Gregory Petrosyan <pgregory@pgregory.net>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#![forbid(unsafe_code)]
+
+mod flags;
+
+fn main() -> anyhow::Result<()> {
+    let flags = flags::Xtask::from_env_or_exit();
+    match flags.subcommand {
+        flags::XtaskCmd::HelloWorld(_) => {
+            println!("Hello, World!");
+            Ok(())
+        }
+    }
+}
