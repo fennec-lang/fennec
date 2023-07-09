09c5545ea5a180ffd0e5d7dec873fda3068373ab
Gregory Petrosyan <pgregory@pgregory.net>
2023-07-09 18:10:10 +0300

    Better version command

--- crates/fennec/src/main.rs
+++ crates/fennec/src/main.rs
@@ -10,21 +10,37 @@ mod flags;
 
 use crate::flags::FennecCmd::Version;
 use env_logger::Env;
+use regex::Regex;
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let flags = flags::Fennec::from_env_or_exit();
 
     let default_level = if flags.verbose { "debug" } else { "info" };
     env_logger::Builder::from_env(Env::default().default_filter_or(default_level)).init();
 
     match flags.subcommand {
-        Version(_) => {
-            println!(
-                "Fennec {}, built by {} at {}",
-                env!("BUILD_GIT_DESCRIBE"),
-                env!("BUILD_RUSTC_VERSION"),
-                env!("BUILD_DATE"),
-            );
-        }
+        Version(_) => print_version(flags.verbose),
     }
+}
+
+fn print_version(verbose: bool) -> anyhow::Result<()> {
+    let desc = env!("BUILD_GIT_DESCRIBE");
+    let version_re = Regex::new(r"^v\d+\.\d+\.\d+")?;
+    let version = if version_re.is_match(desc) {
+        desc.split_at(1).1
+    } else {
+        desc
+    };
+
+    if verbose {
+        println!(
+            "fennec {version}, built by {} at {}",
+            env!("BUILD_RUSTC_VERSION"),
+            env!("BUILD_DATE"),
+        );
+    } else {
+        println!("fennec {version}");
+    }
+
+    Ok(())
 }
