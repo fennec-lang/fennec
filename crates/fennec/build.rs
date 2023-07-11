29f12ec6841c8766afbd71d340e620d814ca7ed5
Gregory Petrosyan <pgregory@pgregory.net>
2023-07-12 00:20:50 +0300

    fennec v0.1.4

--- crates/fennec/build.rs
+++ crates/fennec/build.rs
@@ -8,7 +8,7 @@ use time::format_description::well_known::Rfc2822;
 use time::OffsetDateTime;
 use xshell::{cmd, Shell};
 
-const RELEASE_VERSION: &str = "0.1.3";
+const RELEASE_VERSION: &str = "0.1.4";
 
 fn main() -> anyhow::Result<()> {
     let sh = Shell::new()?;
