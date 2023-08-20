830ebf238dd7fa6979862cf051221e9bea42f2a0
Gregory Petrosyan <pgregory@pgregory.net>
2023-08-20 20:16:08 +0300

    Comments

--- crates/fennec-common/src/types.rs
+++ crates/fennec-common/src/types.rs
@@ -4,8 +4,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-pub struct RootPath(String);
-pub struct FilePath(String);
+pub struct RootPath(String); // UTF-8 + forward slash
+pub struct FilePath(String); // UTF-8 + forward slash
 
 impl RootPath {
     #[must_use]
