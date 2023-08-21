121ea4df57138af80ec21a68e48f6985015869a7
Gregory Petrosyan <pgregory@pgregory.net>
2023-08-22 00:01:49 +0300

    server: prep work for root walkdir

--- crates/fennec-server/src/lib.rs
+++ crates/fennec-server/src/lib.rs
@@ -15,6 +15,7 @@ const FILE_SCHEME: &str = "file";
 pub struct Server {
     conn: lsp_server::Connection,
     io_threads: lsp_server::IoThreads,
+    request_id: i32,
 
     // TODO: use
     pub folders: Vec<RootPath>,
@@ -65,14 +66,10 @@ impl Server {
         conn.initialize_finish(id, init_result)
             .context("failed to send InitializeResult")?;
 
-        register_module_root_watchers(&conn).context(format!(
-            "failed to register {MODULE_ROOT_FILENAME} watchers"
-        ))?;
-        // TODO: we need to find initial set of roots by hand
-
         Ok(Server {
             conn,
             io_threads,
+            request_id: 0,
             utf8_pos,
             folders,
             parent_process_id: init_params.process_id,
@@ -84,22 +81,57 @@ impl Server {
         Ok(())
     }
 
-    // TODO: write real code
+    fn next_id(&mut self) -> lsp_server::RequestId {
+        let id = self.request_id;
+        self.request_id += 1;
+        lsp_server::RequestId::from(id)
+    }
+
     pub fn serve(&mut self) -> Result<(), anyhow::Error> {
+        let reg_id = self.next_id();
+        let mut registered_root_watchers = false;
+        register_module_root_watchers(&self.conn, reg_id.clone()).context(format!(
+            "failed to register {MODULE_ROOT_FILENAME} watchers"
+        ))?;
+
         for msg in &self.conn.receiver {
-            log::trace!("got msg: {msg:?}");
             match msg {
                 lsp_server::Message::Request(req) => {
-                    log::trace!("got request: {req:?}");
                     if self.conn.handle_shutdown(&req)? {
                         return Ok(());
                     }
+                    if !registered_root_watchers {
+                        let lsp_server::Request { id, method, .. } = req;
+                        let msg = format!(
+                            r#"got "{method}" (id {id}) request before root watchers were registered, ignoring"#
+                        );
+                        log::warn!("{msg}");
+                        let _ = self.conn.sender.send(
+                            lsp_server::Response::new_err(
+                                id,
+                                lsp_server::ErrorCode::ContentModified as i32,
+                                msg,
+                            )
+                            .into(),
+                        );
+                    }
                 }
                 lsp_server::Message::Response(resp) => {
-                    log::trace!("got response: {resp:?}");
+                    if resp.id == reg_id {
+                        if let Some(lsp_server::ResponseError { code, message, .. }) = resp.error {
+                            return Err(anyhow!("failed to register {MODULE_ROOT_FILENAME} watchers: [{code}] {message}"));
+                        }
+                        // TODO: walkdir
+                        registered_root_watchers = true;
+                    }
                 }
                 lsp_server::Message::Notification(not) => {
-                    log::trace!("got notification: {not:?}");
+                    if !registered_root_watchers {
+                        let lsp_server::Notification { method, .. } = not;
+                        log::warn!(
+                            r#"got "{method}" notification before root watchers were registered, ignoring"#
+                        );
+                    }
                 }
             }
         }
@@ -143,12 +175,13 @@ fn workspace_roots(init_params: &lsp_types::InitializeParams) -> Vec<RootPath> {
         .collect()
 }
 
-fn register_module_root_watchers(conn: &lsp_server::Connection) -> Result<(), anyhow::Error> {
+fn register_module_root_watchers(
+    conn: &lsp_server::Connection,
+    id: lsp_server::RequestId,
+) -> Result<(), anyhow::Error> {
     let opts = lsp_types::DidChangeWatchedFilesRegistrationOptions {
         watchers: vec![lsp_types::FileSystemWatcher {
-            glob_pattern: lsp_types::GlobPattern::String(
-                format!("**/{MODULE_ROOT_FILENAME}").to_owned(),
-            ),
+            glob_pattern: lsp_types::GlobPattern::String(format!("**/{MODULE_ROOT_FILENAME}")),
             kind: None, // create + change + delete
         }],
     };
@@ -165,14 +198,13 @@ fn register_module_root_watchers(conn: &lsp_server::Connection) -> Result<(), an
     let params = serde_json::to_value(params).context("failed to serialize RegistrationParams")?;
 
     let req = lsp_server::Request {
-        id: lsp_server::RequestId::from(0),
+        id,
         method: lsp_types::request::RegisterCapability::METHOD.to_owned(),
         params,
     };
     conn.sender
-        .send(lsp_server::Message::Request(req))
+        .send(req.into())
         .context("failed to send client/registerCapability request")?;
 
-    // TODO: should we wait for response or no?
     Ok(())
 }
