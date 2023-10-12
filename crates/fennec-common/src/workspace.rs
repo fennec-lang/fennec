2dc0d77807f1d770045cc821bdba0bc9c320c3b7
Gregory Petrosyan <pgregory@pgregory.net>
2023-10-12 11:49:25 +0300

    common: better workspace types

--- crates/fennec-common/src/workspace.rs
+++ crates/fennec-common/src/workspace.rs
@@ -4,48 +4,43 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use std::sync::Arc;
+use std::{path::PathBuf, sync::Arc};
 
 use crate::types;
+
+pub struct File {
+    pub source: PathBuf,
+    pub content: Arc<str>, // empty in case of deleted file
+}
+
+pub struct PackageUpdate {
+    pub source: PathBuf,
+    pub path: types::ImportPath,
+    pub files: Vec<File>,
+    pub update: PackageUpdateKind,
+}
+
+pub enum PackageUpdateKind {
+    PackageAdded,
+    PackageRemoved,
+    PackageUpdated,
+}
 
 pub struct ModuleManifest {
     pub module: types::ImportPath,
     pub fennec: types::FennecVersion,
 }
 
-pub struct Package {
-    pub path: types::ImportPath,
-    pub files: types::HashMap<String, File>, // TODO: replace with Vec?
-}
-
-pub struct File {
-    pub name: String,
-    pub content: Arc<str>, // empty in case of deleted file
-}
-
 pub struct ModuleUpdate {
-    pub path: types::ImportPath,
-    pub data: ModuleUpdateData,
+    pub source: PathBuf,
+    pub module: types::ImportPath,        // same as manifest.module
+    pub manifest: Option<ModuleManifest>, // empty in case of no changes to the manifest
+    pub packages: Vec<PackageUpdate>,
+    pub update: ModuleUpdateKind,
 }
 
-pub enum ModuleUpdateData {
-    ModuleAdded(ModuleManifest),
-    ModuleRemoved, // implicitly means removing all module's packages
-    ModuleManifestUpdated(Vec<ManifestUpdate>),
-    ModulePackagesUpdated(Vec<PackageUpdate>),
-}
-
-pub enum ManifestUpdate {
-    FennecVersionUpdated(String),
-}
-
-pub struct PackageUpdate {
-    pub path: types::ImportPath,
-    pub data: PackageUpdateData,
-}
-
-pub enum PackageUpdateData {
-    PackageAdded(Package),
-    PackageRemoved,
-    PackageUpdated(Vec<File>),
+pub enum ModuleUpdateKind {
+    ModuleAdded,
+    ModuleRemoved,
+    ModuleUpdated,
 }
